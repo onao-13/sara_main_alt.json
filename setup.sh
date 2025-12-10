7866492d5118184412ce3dc48d948c8d00151860
onao13 <onao13@outlook.com>
2025-12-10 12:13:02 +0300

    start comfyui

--- setup.sh
+++ setup.sh
@@ -14,3 +14,12 @@ hf download ananona/dependencies \
 hf download ananona/dependencies \
     WAN2.2-LowNoise_SmartphoneSnapshotPhotoReality_v3_by-AI_Characters.safetensors \
     --local-dir /comfyui/models/lora;
+
+ls -a 
+
+echo "Starting ComfyUI"
+
+python -u /comfyui/main.py --disable-auto-launch --disable-metadata--log-stdout &
+
+echo "Starting ComfyUI Handler"
+python -u /handler.py
